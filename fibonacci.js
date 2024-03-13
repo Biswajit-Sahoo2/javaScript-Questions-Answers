7d4adece1f78d46a9b85aeb374c0f95be5b6347f
Biswajit-Sahoo2 <biswajitnov11@gmail.com>
2024-03-13 18:31:57 +0530

    Initial Commit

--- fibonacci.js
+++ fibonacci.js
@@ -5,12 +5,13 @@ let a = 0;
 let b = 1;
 
 console.log("Fibonacci series :");
-
+//Print the values a & b
 console.log(a + "\n" + b);
 
 for (let i = 2; i < n; i++) {
+    //Add the values of a & b
     let fib = a + b;
-    a = b;
-    b = fib;
+    a = b;  //Store the value of b in a
+    b = fib;  //Store the added value of a & b in b
     console.log(fib);
 }
